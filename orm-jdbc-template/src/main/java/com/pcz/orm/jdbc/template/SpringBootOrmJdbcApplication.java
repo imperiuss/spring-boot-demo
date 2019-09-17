40e865651ef0b953ebee79086050e11f9d2c3ce8
picongzhi <picongzhi@gmail.com>
2019-09-18 02:02:00 +0800

    feat(orm-jdbc-template): add orm jdbc template

---
+++ orm-jdbc-template/src/main/java/com/pcz/orm/jdbc/template/SpringBootOrmJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.pcz.orm.jdbc.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author picongzhi
+ */
+@SpringBootApplication
+public class SpringBootOrmJdbcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOrmJdbcApplication.class, args);
+    }
+}
